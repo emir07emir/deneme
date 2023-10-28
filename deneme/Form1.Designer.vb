bfe61a1782bd5fd5ef8070a42091c5a5c93189e4
iemir <iemir@EMIR07>
2023-10-28 18:22:15 +0300

    Add project files.

---
+++ deneme/Form1.Designer.vb
@@ -0,0 +1,168 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Button1 = New Button()
+        Label1 = New Label()
+        Label2 = New Label()
+        Label3 = New Label()
+        Label4 = New Label()
+        TextBox1 = New TextBox()
+        TextBox2 = New TextBox()
+        TextBox3 = New TextBox()
+        TextBox4 = New TextBox()
+        ListBox1 = New ListBox()
+        Button2 = New Button()
+        SuspendLayout()
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(454, 334)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(94, 39)
+        Button1.TabIndex = 0
+        Button1.Text = "yazdır"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(242, 116)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(110, 20)
+        Label1.TabIndex = 1
+        Label1.Text = "kullanıcı adınız:"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(242, 156)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(59, 20)
+        Label2.TabIndex = 2
+        Label2.Text = "şifreniz:"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(242, 246)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(40, 20)
+        Label3.TabIndex = 3
+        Label3.Text = "şehir"
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Location = New Point(242, 204)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(50, 20)
+        Label4.TabIndex = 4
+        Label4.Text = "tel no:"
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(513, 116)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(125, 27)
+        TextBox1.TabIndex = 5
+        TextBox1.UseWaitCursor = True
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Location = New Point(513, 246)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(125, 27)
+        TextBox2.TabIndex = 8
+        TextBox2.UseWaitCursor = True
+        ' 
+        ' TextBox3
+        ' 
+        TextBox3.Location = New Point(513, 197)
+        TextBox3.Name = "TextBox3"
+        TextBox3.Size = New Size(125, 27)
+        TextBox3.TabIndex = 7
+        TextBox3.UseWaitCursor = True
+        ' 
+        ' TextBox4
+        ' 
+        TextBox4.Location = New Point(513, 156)
+        TextBox4.Name = "TextBox4"
+        TextBox4.Size = New Size(125, 27)
+        TextBox4.TabIndex = 6
+        TextBox4.UseSystemPasswordChar = True
+        TextBox4.UseWaitCursor = True
+        ' 
+        ' ListBox1
+        ' 
+        ListBox1.FormattingEnabled = True
+        ListBox1.ItemHeight = 20
+        ListBox1.Location = New Point(847, 311)
+        ListBox1.Name = "ListBox1"
+        ListBox1.Size = New Size(207, 184)
+        ListBox1.TabIndex = 9
+        ' 
+        ' Button2
+        ' 
+        Button2.Location = New Point(563, 334)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(94, 39)
+        Button2.TabIndex = 10
+        Button2.Text = "temizle"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = SystemColors.Info
+        ClientSize = New Size(1151, 600)
+        Controls.Add(Button2)
+        Controls.Add(ListBox1)
+        Controls.Add(TextBox4)
+        Controls.Add(TextBox3)
+        Controls.Add(TextBox2)
+        Controls.Add(TextBox1)
+        Controls.Add(Label4)
+        Controls.Add(Label3)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Controls.Add(Button1)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents Button2 As Button
+End Class
---
+++ deneme/Form1.vb
@@ -0,0 +1,34 @@
+﻿Public Class Form1
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        ListBox1.Items.Add(TextBox1.Text)
+        ListBox1.Items.Add(TextBox2.Text)
+        ListBox1.Items.Add(TextBox3.Text)
+        ListBox1.Items.Add(TextBox4.Text)
+
+
+
+
+
+
+
+
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        TextBox1.Clear()
+        TextBox2.Clear()
+        TextBox3.Clear()
+        TextBox4.Clear()
+
+
+
+
+
+
+    End Sub
+End Class
